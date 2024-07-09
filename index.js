45e2bb579f374537026eb0d54d8991f18db6e31d
ESTON NDUNGU <estonndungu1@gmail.com>
2024-07-09 14:57:04 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+  ]
+
+  function superbowlWin(record) {
+    
+    let win = record.find(function(entry) {
+      return entry.result === 'W';
+    });
+  
+    
+    if (win) {
+    
+      return win.year;
+    } else {
+      
+      return undefined;
+    }
+  }
